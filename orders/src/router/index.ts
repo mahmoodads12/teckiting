58b136e2e7b5b8bd9a9246f8a768c0f73673f2b4
mahmoud Ad <mahmoodads12@gmail.com>
2025-01-09 07:49:01 +0100

    fixes

--- orders/src/router/index.ts
+++ orders/src/router/index.ts
@@ -2,7 +2,6 @@ import { requireAuth } from '@matickets12/common';
 import express, { Request, Response } from 'express';
 import { Order } from '../models/order';
 
-requireAuth;
 const router = express.Router();
 
 router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
